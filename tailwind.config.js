bb44bccb461339a6cfc77dfb5bb90c8730406521
Rafik El Hadi Houari <rehhouari@gmail.com>
2025-03-16 23:19:44 +0100

    6.2.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-		"./public/views/*.html"
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+		'./public/views/**/*.{html}',
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-}
-
+};
